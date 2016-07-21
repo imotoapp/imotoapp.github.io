c6bd71808d9415c3b43c2f544f0eb57569cf5206
imotoapp <imotoapp@outlook.com>
2016-07-21 19:30:11 +0100

    Add files via upload

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,15 +1,1 @@
-!function() {
-    $(document).on("ready", function() {
-        var t=$(".js-main");
-        $(".js-hamburger").on("click", function() {
-            $(this).toggleClass("is-active"), t.toggleClass("is-showing-mobile-nav")
-        }
-        ),
-        $("img[data-retina]").each(function() {
-            $(this).attr("src", $(this).attr("data-retina"))
-        }
-        )
-    }
-    )
-}
-();
+!function(){$(document).on("ready",function(){var t=$(".js-main");$(".js-hamburger").on("click",function(){$(this).toggleClass("is-active"),t.toggleClass("is-showing-mobile-nav")}),$("img[data-retina]").each(function(){$(this).attr("src",$(this).attr("data-retina"))})})}();
